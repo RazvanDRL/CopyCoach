12df9204eb36035530223038ff56652388188c49
RazvanDRL <razdir97@gmail.com>
2024-10-05 20:37:58 +0300

    update constants

--- constants.ts
+++ constants.ts
@@ -1,56 +1,56 @@
-export type Task = "email_marketing" | "social_media" | "flyer" | "blog_post" | "story_scripts" | "captions";
+export type Task = "blog_post" | "captions" | "email_marketing" | "flyer" | "social_media" | "story_scripts";
 
 export const niches = [
     {
-        value: "local_shops",
-        label: "Local Business",
-        tasks: ["email_marketing", "social_media", "flyer"] as Task[]
+        value: "clinics",
+        label: "Clinics",
+        tasks: ["social_media", "email_marketing", "flyer"] as Task[]
     },
     {
         value: "e-commerce",
         label: "E-commerce",
-        tasks: ["email_marketing", "social_media", "blog_post"] as Task[]
+        tasks: ["blog_post", "social_media", "email_marketing", "product_description"] as Task[]
     },
     {
         value: "influencers",
         label: "Influencers",
-        tasks: ["social_media", "story_scripts", "captions"] as Task[]
+        tasks: ["story_scrips", "blog_posts", "captions", "scripts", "story_scripts", "email_marketing", "social_media"] as Task[]
     },
     {
-        value: "clinics",
-        label: "Clinics",
-        tasks: ["email_marketing", "social_media", "flyer"] as Task[]
+        value: "local_shops",
+        label: "Local Business",
+        tasks: ["email_marketing", "flyer", "social_media"] as Task[]
     },
     {
         value: "restaurants",
         label: "Restaurants",
-        tasks: ["social_media", "flyer", "captions"] as Task[]
+        tasks: ["email_marketing", "social_media", "flyer"] as Task[]
     },
 ];
 
 export const tasks: { value: Task; label: string }[] = [
     {
-        value: "email_marketing",
-        label: "Email Marketing",
+        value: "blog_post",
+        label: "Blog Post",
     },
     {
-        value: "social_media",
-        label: "Social Media",
+        value: "captions",
+        label: "Captions",
+    },
+    {
+        value: "email_marketing",
+        label: "Email Marketing",
     },
     {
         value: "flyer",
         label: "Flyer",
     },
     {
-        value: "blog_post",
-        label: "Blog Post",
+        value: "social_media",
+        label: "Social Media",
     },
     {
         value: "story_scripts",
         label: "Story Scripts",
-    },
-    {
-        value: "captions",
-        label: "Captions",
     },
 ]
