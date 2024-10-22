06c811469608f0ced66c4393bc5e32d2861e8830
RazvanDRL <razdir97@gmail.com>
2024-10-23 00:22:40 +0300

    design small tweaks landing

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ const examples = [
             "Track your progress",
             "Unlock surprise benefits with each level",
             "Access exclusive opportunities",
-        ],  
+        ],
         image: CeciuAvatar,
     },
 ]
@@ -111,21 +111,21 @@ const withCopyCoach = [{
 
 const howItWorks = [
     {
-        title: "Exercise",
+        title: "Practice Your Skills",
         description: "Work on realistic copywriting exercises designed to improve your skills in a practical way.",
     },
     {
-        title: "Get Feedback",
+        title: "Get Smart Feedback",
         description: "Receive detailed feedback on your copy, with specific suggestions for improvement and personalized tips.",
     },
     {
-        title: "Improve",
+        title: "Level Up Fast",
         description: "Track your progress and continuously improve your copywriting skills with each exercise.",
     }
 ]
 
 const LandingPage: React.FC = () => {
-    const [selectedOption, setSelectedOption] = useState(1);
+    const [selectedOption, setSelectedOption] = useState<1 | 2 | 3>(1);
 
     return (
         <>
@@ -138,12 +138,12 @@ const LandingPage: React.FC = () => {
                 {/* Section - landing */}
                 <section className="flex flex-col items-center justify-center min-h-[85dvh] relative overflow-hidden">
                     {/* Background symbols */}
-                    <div className="absolute inset-0 z-0 opacity-25">
-                        <Pen className="absolute top-1/4 left-1/4 h-10 w-10 sm:h-16 sm:w-16 text-[#6CB4EE] transform -rotate-12" />
-                        <Book className="absolute top-1/3 right-1/4 h-10 w-10 sm:h-16 sm:w-16 text-[#6CB4EE] transform rotate-6" />
+                    {/* <div className="absolute inset-0 z-0 opacity-25">
+                        <Pen className="absolute top-1/4 left-[10%] h-10 w-10 sm:h-16 sm:w-16 text-[#6CB4EE] transform -rotate-12" />
+                        <Book className="absolute top-1/3 right-[10%] h-10 w-10 sm:h-16 sm:w-16 text-[#6CB4EE] transform rotate-6" />
                         <Sparkles className="absolute bottom-1/4 left-1/3 h-8 w-8 sm:h-12 sm:w-12 text-[#6CB4EE]" />
                         <Pen className="absolute bottom-1/3 right-[20%] lg:right-1/3 h-10 w-10 sm:h-14 sm:w-14 text-[#6CB4EE] transform rotate-90" />
-                    </div>
+                    </div> */}
                     <div className="mt-12 flex flex-col items-center justify-center gap-10 md:gap-12 z-50">
                         <h1 className={`${BricolageGrotesque.className} bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 text-[3.2rem] sm:text-6xl md:text-7xl lg:text-8xl relative font-bold`}>
                             Learn by <span className='underline decoration-dashed decoration-[#007FFF] underline-offset-8'>doing</span>
@@ -162,26 +162,24 @@ const LandingPage: React.FC = () => {
                             </Button>
                         </Link>
                         {/* Social proof - single person */}
-                        <div className="flex flex-col items-center justify-center space-y-4 md:space-y-6">
+                        <div className="flex flex-col items-center justify-center space-y-2 md:space-y-6">
                             <div className="flex items-center justify-center space-x-0.5">
-                                <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
-                                <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
-                                <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
-                                <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
-                                <Star className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
+                                {[...Array(5)].map((_, index) => (
+                                    <Star key={index} className="w-3 h-3 sm:w-4 sm:h-4 fill-[#FFBF00] text-[#FFBF00]" />
+                                ))}
                             </div>
-                            <p className="text-center text-xs sm:text-sm font-medium">
+                            <p className="text-center text-xs sm:text-sm font-medium text-black/70">
                                 {"Amazing. Best copywriting platform I've found."}
                             </p>
                             {/* image + name + followers count */}
-                            <div className="flex items-center justify-center space-x-4">
-                                <Image src={CeciuAvatar} alt="ceciu" width={36} height={36} className="rounded-full sm:w-12 sm:h-12" />
+                            <div className="flex items-center justify-center space-x-4 text-black/50">
+                                <Image src={CeciuAvatar} alt="ceciu" width={140} height={140} className="w-12 h-12 rounded-full aspect-square object-cover" />
                                 <div className="flex flex-col items-left space-y-1 z-50">
-                                    <Link href="https://www.instagram.com/ceciu.gmi/" className="cursor-pointer hover:underline text-left text-xs sm:text-sm font-semibold">
-                                        Ceciu Sebasitan
+                                    <Link href="https://www.instagram.com/ceciu.gmi/" className="cursor-pointer hover:underline text-left text-xs sm:text-sm font-semibold ">
+                                        Tirsin Bianca
                                     </Link>
                                     <p className="text-left text-xs sm:text-sm flex items-center">
-                                        654 followers
+                                        70k followers
                                     </p>
                                 </div>
                             </div>
@@ -242,7 +240,7 @@ const LandingPage: React.FC = () => {
                             <Image src={CeciuAvatar} alt="ceciu" width={36} height={36} className="rounded-full sm:w-12 sm:h-12" />
                             <div className="flex flex-col items-left space-y-1 z-50">
                                 <Link href="https://www.instagram.com/ceciu.gmi/" className="cursor-pointer hover:underline text-left text-xs sm:text-sm font-semibold">
-                                    Ceciu Sebasitan
+                                    Tirsin Bianca
                                 </Link>
                                 <p className="text-left text-xs sm:text-sm flex items-center">
                                     654 followers
@@ -251,99 +249,115 @@ const LandingPage: React.FC = () => {
                         </div>
                     </div>
                     {/* cta */}
-                    <Link href="/signup" className="mt-16">
-                        <Button className={`${BricolageGrotesque.className} bg-[#007FFF] scale-125 sm:scale-150 hover:bg-[#007FFF] text-sm sm:text-base md:text-lg py-3 sm:py-4 md:py-6 px-4 sm:px-6 md:px-8 text-white font-bold rounded-lg shadow-lg transform hover:scale-110 sm:hover:scale-125 transition-all duration-200`}>
+                    <Link href="/signup">
+                        <Button
+                            className={`mt-16 ${BricolageGrotesque.className} bg-[#007FFF] hover:bg-[#007FFF] hover:scale-105 transition-all duration-200 text-2xl md:text-3xl font-bold h-[4rem] md:h-[4.5rem] rounded-xl px-8 md:px-12`}
+                        >
                             Start now
                         </Button>
                     </Link>
                 </section>
 
                 {/* Section - How it works */}
-                <section className="flex flex-col items-center justify-center mt-24">
-                    <h2 className={`${BricolageGrotesque.className} text-center text-3xl sm:text-4xl md:text-5xl font-black`}>
-                        How it works
+                <section className="flex flex-col items-center justify-center mt-[6rem] sm:mt-[10rem] mx-auto px-4 sm:px-6">
+                    <h2 className={`${BricolageGrotesque.className} text-center text-2xl sm:text-4xl md:text-5xl font-black leading-tight`}>
+                        Get started in just three simple steps
                     </h2>
-                    <div className="flex flex-col sm:flex-row gap-6 mt-8 w-full">
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mt-12 sm:mt-16 w-full">
                         {howItWorks.map((item, index) => (
-                            <div key={index} className="flex flex-col items-left text-left justify-center p-6 sm:p-8 bg-white rounded-xl w-full">
-                                <h3 className="text-lg sm:text-xl font-bold">{item.title}</h3>
-                                <p className="mt-2 text-sm sm:text-base text-neutral-700">{item.description}</p>
+                            <div key={index} className="flex flex-col items-left text-left justify-start bg-white rounded-xl w-full h-[250px] p-6 sm:p-8">
+                                <div className="flex items-left font-black text-[#007FFF] text-4xl sm:text-5xl mb-3 sm:mb-4">
+                                    {index + 1}
+                                </div>
+                                <h3 className="text-base sm:text-xl font-bold">{item.title}</h3>
+                                <p className="mt-2 text-sm sm:text-base text-black/50">{item.description}</p>
                             </div>
                         ))}
                     </div>
+                    <Link href="/signup" className="mt-16">
+                        <Button
+                            className={`${BricolageGrotesque.className} bg-[#007FFF] hover:bg-[#007FFF] hover:scale-105 transition-all duration-200 text-xl md:text-2xl font-bold h-[3.5rem] md:h-[4rem] rounded-xl px-8 md:px-10`}
+                        >
+                            Start now
+                        </Button>
+                    </Link>
                 </section>
 
                 {/* Section - Features */}
-                <section className="flex flex-col items-center justify-center mt-24">
+                <section className="flex flex-col items-center justify-center mt-[10rem]">
                     <h2 className={`${BricolageGrotesque.className} text-center text-3xl sm:text-4xl md:text-5xl font-black`}>
                         Automate boring problems.
                         <br />
                         Focus on what really matters.
                     </h2>
-                    {/* menu with 4 options */}
-                    <div className="mt-16 flex items-center justify-center gap-2 sm:gap-6 w-full">
-                        <Button
-                            className={`p-4 sm:p-8 rounded-xl ${selectedOption === 1 ? 'text-[#007FFF] bg-[#007FFF]/10 hover:text-[#007FFF] hover:bg-[#007FFF]/10' : 'hover:text-[#007FFF]/70 hover:bg-[#007FFF]/10'}`}
-                            variant='ghost'
-                            onClick={() => {
-                                setSelectedOption(1);
-                            }}
-                        >
-                            <Pen className="w-4 h-4 sm:w-6 sm:h-6" />
-                        </Button>
-                        <Button
-                            className={`p-4 sm:p-8 ${selectedOption === 2 ? 'text-[#007FFF] bg-[#007FFF]/10 hover:text-[#007FFF] hover:bg-[#007FFF]/10' : 'hover:text-[#007FFF]/70 hover:bg-[#007FFF]/10'}`}
-                            variant='ghost'
-                            onClick={() => {
-                                setSelectedOption(2);
-                            }}
-                        >
-                            <Book className="w-4 h-4 sm:w-6 sm:h-6" />
-                        </Button>
-                        <Button
-                            className={`p-4 sm:p-8 ${selectedOption === 3 ? 'text-[#007FFF] bg-[#007FFF]/10 hover:text-[#007FFF] hover:bg-[#007FFF]/10' : 'hover:text-[#007FFF]/70 hover:bg-[#007FFF]/10'}`}
-                            variant='ghost'
-                            onClick={() => {
-                                setSelectedOption(3);
-                            }}
-                        >
-                            <Mail className="w-4 h-4 sm:w-6 sm:h-6" />
-                        </Button>
-                        <Button
-                            className={`p-4 sm:p-8 ${selectedOption === 4 ? 'text-[#007FFF] bg-[#007FFF]/10 hover:text-[#007FFF] hover:bg-[#007FFF]/10' : 'hover:text-[#007FFF]/70 hover:bg-[#007FFF]/10'}`}
-                            variant='ghost'
-                            onClick={() => {
-                                setSelectedOption(4);
-                            }}
-                        >
-                            <FileText className="w-4 h-4 sm:w-6 sm:h-6" />
-                        </Button>
-                    </div>
-                    <div className="mt-16 flex items-start justify-between w-[99.1vw] bg-[#007FFF] p-6 sm:p-12">
-                        <div className="max-w-5xl mx-auto flex flex-col sm:flex-row">
-                            <div className="flex-1 pr-0 sm:pr-8 max-w-full sm:max-w-sm mb-8 sm:mb-0">
-                                <h1 className="text-xl sm:text-2xl font-bold text-white">{examples[selectedOption - 1].title}</h1>
-                                <p className="text-sm sm:text-md text-white/70 mt-4">{examples[selectedOption - 1].description}</p>
-                                <ul className="text-sm sm:text-md text-white mt-6">
-                                    {examples[selectedOption - 1].benefits.map((benefit, index) => (
-                                        <li key={index} className="flex items-center">
-                                            <svg className="w-3 h-3 sm:w-4 sm:h-4 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
-                                            </svg>
-                                            {benefit}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <div className="flex-shrink-0">
-                                <Image
-                                    src={examples[selectedOption - 1].image}
-                                    alt={examples[selectedOption - 1].title}
-                                    width={300}
-                                    height={600}
-                                    className="rounded-lg shadow-lg w-full sm:w-auto"
-                                />
-                            </div>
+                    <div className="grid grid-cols-1 items-stretch gap-8 sm:gap-12 lg:grid-cols-2 lg:gap-20 mt-16 max-w-7xl mx-auto px-4">
+                        <ul className="w-full">
+                            <li>
+                                <button
+                                    className="group relative flex gap-5 items-center w-full py-5 text-base font-medium text-left md:text-lg"
+                                    onClick={() => setSelectedOption(1)}
+                                    aria-expanded={selectedOption === 1}
+                                >
+                                    <span className="scale-150 duration-100 text-[#007FFF]">
+                                        <Pen className="w-4 h-4 sm:w-6 sm:h-6" />
+                                    </span>
+                                    <span className="group-hover:translate-x-1 duration-150 flex-1 text-base-content text-[#007FFF] font-semibold">
+                                        <h3 className="inline">{examples[0].title}</h3>
+                                    </span>
+                                </button>
+                                <div className={`transition-all duration-300 ease-in-out overflow-hidden ${selectedOption === 1 ? 'opacity-100' : 'opacity-0 max-h-0'}`}>
+                                    <div className="pb-5 leading-relaxed text-black/50">
+                                        {examples[0].description}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button
+                                    className="group relative flex gap-5 items-center w-full py-5 text-base font-medium text-left md:text-lg"
+                                    onClick={() => setSelectedOption(2)}
+                                    aria-expanded={selectedOption === 2}
+                                >
+                                    <span className="scale-150 duration-100">
+                                        <Book className="w-4 h-4 sm:w-6 sm:h-6" />
+                                    </span>
+                                    <span className="group-hover:translate-x-1 duration-150 flex-1 text-base-content">
+                                        <h3 className="inline">{examples[1].title}</h3>
+                                    </span>
+                                </button>
+                                <div className={`transition-all duration-300 ease-in-out overflow-hidden ${selectedOption === 2 ? 'opacity-100' : 'opacity-0 max-h-0'}`}>
+                                    <div className="pb-5 leading-relaxed text-black/50">
+                                        {examples[1].description}
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <button
+                                    className="group relative flex gap-5 items-center w-full py-5 text-base font-medium text-left md:text-lg"
+                                    onClick={() => setSelectedOption(3)}
+                                    aria-expanded={selectedOption === 3}
+                                >
+                                    <span className="scale-150 duration-100">
+                                        <Mail className="w-4 h-4 sm:w-6 sm:h-6" />
+                                    </span>
+                                    <span className="group-hover:translate-x-1 duration-150 flex-1 text-base-content">
+                                        <h3 className="inline">{examples[2].title}</h3>
+                                    </span>
+                                </button>
+                                <div className={`transition-all duration-300 ease-in-out overflow-hidden ${selectedOption === 3 ? 'opacity-100' : 'opacity-0 max-h-0'}`}>
+                                    <div className="pb-5 leading-relaxed text-black/50">
+                                        {examples[2].description}
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div className="rounded-box aspect-square w-full sm:w-[26rem] sm:-m-2 sm:p-2 border-2 border-[#007FFF]/10 bg-base-200">
+                            <Image
+                                src={examples[selectedOption - 1].image}
+                                alt={examples[selectedOption - 1].title}
+                                width={500}
+                                height={500}
+                                className="rounded-lg w-full h-full object-cover"
+                            />
                         </div>
                     </div>
                 </section>
